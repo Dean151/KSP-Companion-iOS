5c9f0b574d264b85b3cc8f1a7b9e7da12d85e8d0
Dean151 <thomas.durand.verjat@gmail.com>
2015-09-30 08:53:05 -0400

    Performing changes on view will appears to refresh data in case a setting has changed

--- KSPCompanion/ViewControllers/KSPSplitViewController.swift
+++ KSPCompanion/ViewControllers/KSPSplitViewController.swift
@@ -19,6 +19,14 @@ class KSPSplitViewController: UISplitViewController, UISplitViewControllerDelega
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "sizeChanged:", name: UIDeviceOrientationDidChangeNotification, object: nil)
     }
     
+    override func viewWillAppear(animated: Bool) {
+        self.viewControllers.forEach { vc in
+            vc.viewWillAppear(animated)
+        }
+        
+        super.viewWillAppear(animated)
+    }
+    
     override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
         self.sizeChanged(self)
     }
@@ -33,5 +41,15 @@ class KSPSplitViewController: UISplitViewController, UISplitViewControllerDelega
         guard let navVC = self.viewControllers.first as? UINavigationController else { return }
         guard let detailVC = navVC.topViewController as? CelestialsTableViewController else { return }
         detailVC.tableView.reloadData()
+    }
+}
+
+extension UINavigationController {
+    override public func viewWillAppear(animated: Bool) {
+        self.viewControllers.forEach { vc in
+            vc.viewWillAppear(animated)
+        }
+        
+        super.viewWillAppear(animated)
     }
 }
